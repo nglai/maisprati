4fb957ac6d8d80bfddc70b850eed3735b66fb7d5
nglai <nglaikoin@hotmail.com>
2024-06-01 22:25:49 -0300

    questoes 33 a 35 + readme

--- Exercicio02/questao31a35.js
+++ Exercicio02/questao31a35.js
@@ -61,10 +61,82 @@
  * 33. Faça um algoritmo que leia uma matriz 3 x 3 e após a leitura, multiplique os elementos da diagonal principal com a média dos elementos da diagonal secundária.
 */
 
+// let matriz = [];
+// for(let i = 0; i < 3; i++){
+//     matriz[i] = [];
+//     for(let j = 0; j < 3; j++){
+//         matriz[i][j] = Math.floor(Math.random() * 10);
+//     };
+// };
+// console.log(matriz)
+// let soma = 0;
+// for (let i = 0; i < 3; i++) {
+//     soma += matriz[i][2 - i];
+// }
+// let mediaDiagonalSecundaria = soma/3;
+// console.log(`Média dos elementos da diagonal secundária: ${mediaDiagonalSecundaria}`);
+// let multiplicacaoDiagonalPrincipal = 1;
+// for (let i = 0; i < 3; i++) {
+//     multiplicacaoDiagonalPrincipal *= matriz[i][i];
+// }
+// console.log(`Multiplicacao dos elementos da diagonal principal: ${multiplicacaoDiagonalPrincipal}`)
+// console.log(`Resultado da multiplicacao da média dos elementos da diagonal secundária pelos elementos da diagonal principal: ${(mediaDiagonalSecundaria * multiplicacaoDiagonalPrincipal).toFixed(2)}`);
+
+
 /**
  * 34. Faça um algoritmo que leia uma matriz 50 x 50 de números reais. A seguir, multiplique cada linha pelo elemento da diagonal principal daquela linha. Mostre a matriz após as multiplicações.
 */
 
+// let matriz = [];
+// for(let i = 0; i < 50; i++){
+//     matriz[i] = [];
+//     for(let j = 0; j < 50; j++){
+//         matriz[i][j] = Math.floor(Math.random() * 10 + 1);
+//     };
+// };
+// let matrizNova = [];
+// for(let i = 0; i < 50; i++){
+//     matrizNova[i] = [];
+//     for(let j = 0; j < 50; j++){
+//         matrizNova[i][j] = matriz[i][j] * matriz[i][i];
+//     };
+// };
+// console.log("Matriz Original",matriz);
+// console.log("Matriz Nova", matrizNova);
+
+
 /**
  * 35. Elaborar um algoritmo que leia um conjunto de 30 valores e os coloca em 2 vetores conforme forem pares ou ímpares. O tamanho do vetor é de 5 posições. Se algum vetor estiver cheio, escrevê-lo. Terminada a leitura, escrever o conteúdo dos dois vetores. Cada vetor pode ser preenchido quantas vezes forem necessárias.
 */
+
+// const prompt = require("prompt-sync")();
+// let vetorImpar = [];
+// let vetorPar = [];
+// function verificaCheio (vetor) {
+//     if (vetor.length >= 5){
+//         console.log("vetor está cheio!")
+//         return true;
+//     } else {return false;}
+// }
+
+// for (let i = 0; i < 30; i++) {
+//     let valor = parseInt(prompt(`Digite o ${i + 1}º valor:`));
+
+//     if (valor % 2 === 0) {
+//         if (!verificaCheio(vetorPar)) {
+//             vetorPar.push(valor);
+//         } else {
+//             console.log("Vetor de pares está cheio:", vetorPar);
+//             vetorPar = [];
+//         }
+//     } else {
+//         if (!verificaCheio(vetorImpar)) {
+//             vetorImpar.push(valor);
+//         } else {
+//             console.log("Vetor de ímpares está cheio:", vetorImpar);
+//             vetorImpar = []
+//         }
+//     }
+// }
+// console.log("Vetor de pares:", vetorPar);
+// console.log("Vetor de impares:", vetorImpar);
---
+++ Exercicio02/readme.md
@@ -0,0 +1,30 @@
+# Instruções
+
+## Instalação dos pacotes necessários
+
+Para o usuário conseguir digitar algo, foi utilizada a biblioteca prompt-sync
+```
+npm i prompt-sync
+```
+
+## Importação da biblioteca
+
+Para utilizar a biblioteca instalada, é necessário sua importação:
+```
+const prompt = require("prompt-sync")();
+```
+
+## Clone do repositório
+
+Para quem vai clonar o repositório, é necessário o comando para baixar as dependencias do package.json:
+```
+npm i
+```
+
+## Execução do exercício
+
+Para facilitar a organização, os exercícios foram divididos em arquivos com 5 questões cada, menos o penultimo arquivo que contém 4 questões e o último arquivo com 1 questão.
+Para executar o exercício, é necessario entrar na pasta do exercicio no terminal (Exercício02), descomentar a resolução da questão que se quer executar e executar o comando no terminal node + nomeDoArquivo. Exemplo:
+```
+node .\questao01a05.js 
+```
